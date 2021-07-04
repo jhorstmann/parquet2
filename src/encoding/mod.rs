fc267a52e88f29c45c2a08b038d7ad1558511478
Jörn Horstmann <git@jhorstmann.net>
2021-07-04 19:24:56 +0200

    Tests and fix for log2 function

--- src/encoding/mod.rs
+++ src/encoding/mod.rs
@@ -19,21 +19,35 @@ pub fn get_length(values: &[u8]) -> u32 {
 
 /// Returns floor(log2(x))
 #[inline]
-pub fn log2(mut x: u64) -> u32 {
-    if x == 1 {
-        return 1;
-    }
-    x -= 1;
-    let mut result = 0;
-    while x > 0 {
-        x >>= 1;
-        result += 1;
-    }
-    result
+pub fn log2(x: u64) -> u32 {
+    64 - x.leading_zeros()
 }
 
 /// Returns the ceil of value/divisor
 #[inline]
 pub fn ceil8(value: usize) -> usize {
     value / 8 + ((value % 8 != 0) as usize)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::log2;
+
+    #[test]
+    fn test_log2() {
+        assert_eq!(0, log2(0));
+        assert_eq!(1, log2(1));
+        assert_eq!(2, log2(2));
+        assert_eq!(2, log2(3));
+        assert_eq!(3, log2(4));
+        assert_eq!(3, log2(5));
+        assert_eq!(3, log2(6));
+        assert_eq!(3, log2(7));
+        assert_eq!(4, log2(8));
+        assert_eq!(4, log2(15));
+
+        assert_eq!(8, log2(255));
+        assert_eq!(9, log2(256));
+    }
 }
